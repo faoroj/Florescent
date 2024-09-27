48c63e820b5dfb28322c53d6f0b02eb6ce8bd20b
faoroj <jfaoro15@gmail.com>
2024-09-27 06:05:15 -0400

    Login Screen Start

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
+import CustomButton from '../../components/CustomButton'
+import { Link } from 'expo-router'
 
-const Signin = () => {
+const SignIn = () => {
+
+  const [form, setform] = useState({
+    email: '',
+    password:''
+  })
+
+  const [isSubmitting, setisSubmitting] = useState(false)
+
+  const submit = () => {
+
+  }
   return (
-    <View>
-      <Text>Signin</Text>
-    </View>
+    <SafeAreaView className="h-full">
+      <ScrollView>
+        <View className="w-full flex justify-start min-h-[85vh] px-4 my-6">
+          <Text className="text-[42px] font-regular">Welcome Back!</Text>
+          <Text className="text-primary-100 text-[26px]">Sign in to continue</Text>
+
+
+
+
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
-export default Signin
+export default SignIn
