02ab788c4552d957237f12fc0db82ce17adb5f77
faoroj <jfaoro15@gmail.com>
2024-09-27 21:01:38 -0400

    sign-in page prog2

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,11 +3,18 @@ import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images, icons } from '../../constants'
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import SocialMediaButton from '../../components/SocialMediaButton'
+import { Link, router } from 'expo-router'
 import FormField from '../../components/FormField'
 import * as Linking from 'expo-linking';
 
 const SignIn = () => {
+
+  const socialMediaLinks = [
+    { icon: icons.xicon, url: 'https://www.twitter.com' },
+    { icon: icons.googleicon, url: 'https://www.google.com' },
+    { icon: icons.facebookicon, url: 'https://www.facebook.com' }
+  ];
 
   const [form, setform] = useState({
     username: '',
@@ -32,7 +39,7 @@ const SignIn = () => {
               value={form.username}
               placeholder="Username"
               handleChangeText={(text) => setform(text)}
-              otherStyles="mt-[90px]"
+              otherStyles="mt-[70px]"
             />
 
             <FormField
@@ -49,39 +56,25 @@ const SignIn = () => {
               isLoading={isSubmitting}
             />
 
-            <Text className="text-[12px] font-medium mt-3">Forgot Password?</Text>
+            <TouchableOpacity onPress={() => {/* navigate to forgot password */}}>
+              <Text className="text-[12px] font-medium mt-3 text-primary-200">Forgot Password?</Text>
+            </TouchableOpacity>
 
             <View className="w-screen px-1 py-2 flex flex-row justify-center items-center border-b mt-[60px]"></View>
 
-            <View className="flex flex-row w-full justify-evenly items-center mt-9">
-
-              <TouchableOpacity onPress={() => Linking.openURL('https://www.twitter.com')}>
-                <Image
-                  source={icons.xicon}
-                  className="w-[41px] h-[47px]"
-                  resizeMode='contain'
-                />
-              </TouchableOpacity>
-
-              <TouchableOpacity onPress={() => Linking.openURL('https://www.google.com')}>
-                <Image
-                  source={icons.googleicon}
-                  className="w-[41px] h-[47px]"
-                  resizeMode='contain'
-                />
-              </TouchableOpacity>
-
-              <TouchableOpacity onPress={() => Linking.openURL('https://www.facebook.com')}>
-                <Image
-                  source={icons.facebookicon}
-                  className="w-[41px] h-[47px]"
-                  resizeMode='contain'
-                />
-              </TouchableOpacity>
-
+            <View className="flex flex-row w-full justify-around items-center mt-8">
+              {socialMediaLinks.map((item, index) => (
+                <SocialMediaButton key={index} iconSource={item.icon} url={item.url}/>
+              ))}
             </View>
-
-            <Text className="mt-3">Don't have an account? <Text className="text-primary-200 font-semibold">Sign up now!</Text></Text>
+            
+            <View className="mt-4 flex-row">
+              <Text className="text-black">Don't have an account? 
+                <TouchableOpacity onPress={() => router.push('/sign-up')}>
+                  <Text className="text-primary-200 font-semibold">Sign up now!</Text>
+                </TouchableOpacity>
+              </Text>
+            </View>
 
           </View>
         </View>
---
+++ components/SocialMediaButton.jsx
@@ -0,0 +1,26 @@
+import { TouchableOpacity, View, Image } from 'react-native'
+import React from 'react'
+
+const SocialMediaButton = ({ iconSource, url }) => {
+    return (
+      <View className="w-[70px] h-[70px] rounded-full bg-white justify-center items-center"
+        style={{
+          shadowColor: 'black', // Set the shadow color to black
+          shadowOpacity: 0.25,  // Set the shadow opacity to 25%
+          shadowOffset: { width: 0, height: 1 }, // X: 0, Y: 1
+          shadowRadius: 7.8, // Blur radius of 7.8
+          elevation: 8, // Elevation for Android shadow (if required)
+        }}
+      >
+        <TouchableOpacity onPress={() => Linking.openURL(url)}>
+          <Image
+            source={iconSource}
+            className="w-[41px] h-[47px]"
+            resizeMode='contain'
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  };
+  export default SocialMediaButton
+  
